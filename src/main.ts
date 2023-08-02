9bf13b7c8e53f86e9e80beab491b6bf81ab66af6
shahzod-rahimov <rakhimovsh3235@gmail.com>
2023-08-02 07:22:43 +0500

    test

--- src/main.ts
+++ src/main.ts
@@ -173,12 +173,12 @@ async function run(): Promise<void> {
         };
 
         if (event === "pull_request") {
+            throw new Error(JSON.stringify(telegramOptions));
             telegramOptions.message_thread_id = telegram_topic;
             telegramOptions.reply_markup = { keyboard: [[{ text: "github", url: data.pull_req_url }]] };
         }
         throw new Error(JSON.stringify(telegramOptions));
 
-        
         //send message via telegram
         await axios.post(`https://api.telegram.org/bot${telegram_token}/sendMessage`, telegramOptions);
     } catch (error: any) {
